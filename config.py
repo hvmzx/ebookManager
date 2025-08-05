6dfb75d26ecc46d1581f7f5865bb5428953dfd11
hvmzx <hamza.mebarek1@gmail.com>
2025-08-05 09:58:36 +0200

    feat: fixed boolean env not being passed properly

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # config.py
 import os
 
-book_monitoring = os.getenv('BOOK_MONITORING', 'false')
-manga_monitoring = os.getenv('MANGA_MONITORING', 'false')
+book_monitoring = os.getenv('BOOK_MONITORING', 'false').lower() == 'true'
+manga_monitoring = os.getenv('MANGA_MONITORING', 'false').lower() == 'true'
 scan_schedule = os.getenv('SCAN_SCHEDULE', '* * * * *')
 max_threads = int(os.getenv('MAX_THREADS', 4))
 hardcover_api_key = os.getenv('HARDCOVER_API_KEY', '')
-book_update_metadata = os.getenv('BOOK_UPDATE_METADATA', 'false')
-manga_update_metadata = os.getenv('MANGA_UPDATE_METADATA', 'false')
-kcc_process = os.getenv('KCC_PROCESS', 'false')
+book_update_metadata = os.getenv('BOOK_UPDATE_METADATA', 'false').lower() == 'true'
+manga_update_metadata = os.getenv('MANGA_UPDATE_METADATA', 'false').lower() == 'true'
+kcc_process = os.getenv('KCC_PROCESS', 'false').lower() == 'true'
 kcc_options = os.getenv('KCC_OPTIONS', '')
 update_mode = os.getenv('UPDATE_MODE', 'PARTIAL')
-remove_prefix = os.getenv('REMOVE_PREFIX', 'false')
-clean_title = os.getenv('CLEAN_TITLE', 'false')
+remove_prefix = os.getenv('REMOVE_PREFIX', 'false').lower() == 'true'
+clean_title = os.getenv('CLEAN_TITLE', 'false').lower() == 'true'
 
 watch_directory = '/app/ebooks/ebooks_in'
 output_directory = '/app/ebooks/ebooks_out'
